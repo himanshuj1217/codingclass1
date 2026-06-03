23c6791467c6433a5ffc863f4967a55af27db6f7
himanshuj <hima.jag1217@gmail.com>
2026-06-03 16:59:55 -0400

    class 3 work 2

--- classwork/class 3 work.py
+++ classwork/class 3 work.py
@@ -3,3 +3,5 @@ if age>=18:
     print("adult")
 else: 
     print("you are not an adult")
+# write a program, that takes age value from user and then it displays 
+# adult when the age is greater equal to 18 else not adult
