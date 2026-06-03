71c1e32a241fd23968f93fe33195753116408f7c
himanshuj <hima.jag1217@gmail.com>
2026-06-03 15:49:36 -0400

    Assignment 2n4

---
+++ Assignments/git Assignment 2.4.py
@@ -0,0 +1,14 @@
+print(ord("A")) #A = 65 
+print(ord("Z")) #Z=90
+#Z is greater than A 
+print(ord("a")) #a=97
+#a>Z = true
+print(ord("K")) #k=75
+print(ord("D")) #D=68
+#K>D = true
+print(ord("m")) #m=109
+print(ord("p")) #p=112
+#m<p = true
+print(ord("z")) # z = 122
+print(ord("x")) # x = 120
+#z>x=true
